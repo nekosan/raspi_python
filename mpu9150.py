7d4c5d61416782813019c82d108d5207fae7ab02
Nekosan <neko3.raven@gmail.com>
2014-06-26 13:39:50 +0900

    ゼロ割の例外入れた

--- mpu9150.py
+++ mpu9150.py
@@ -32,7 +32,7 @@ class Mpu9150:
         self.acc[1] += self.read(0x3e)
         self.acc[2] = self.read(0x3f) << 8
         self.acc[2] += self.read(0x40)
-	for i in range(len(self.acc)) :
+    for i in range(len(self.acc)) :
             if self.acc[i] >= 32768:
                 self.acc[i] -= 65536
             self.acc[i] = float(self.acc[i])
@@ -43,22 +43,27 @@ class Mpu9150:
         self.gyr[1] += self.read(0x44)
         self.gyr[2] = self.read(0x45) << 8
         self.gyr[2] += self.read(0x46)
-	for i in range(len(self.gyr)) :
+    for i in range(len(self.gyr)) :
             if self.gyr[i] >= 32768:
                 self.gyr[i] -= 65536
             self.gyr[i] = float(self.gyr[i])
 
     def calc_acc_deg(self):
         acc_deg = []
-        acc_deg.append(M.asin(self.acc[0] / M.sqrt(self.acc[0] ** 2 + self.acc[2] ** 2)))
-        acc_deg.append(M.asin(self.acc[1] / M.sqrt(self.acc[1] ** 2 + self.acc[2] ** 2)))
-        return acc_deg
+        try:
+            acc_deg.append(M.asin(self.acc[0] / M.sqrt(self.acc[0] ** 2 + self.acc[2] ** 2)) * (180.0 / M.pi))
+            acc_deg.append(M.asin(self.acc[1] / M.sqrt(self.acc[1] ** 2 + self.acc[2] ** 2)) * (180.0 / M.pi))
+        except ZeroDivisionError:
+            return 1
+        else :
+            return acc_deg
 
 if __name__ == '__main__':
     mpu = Mpu9150()
 
     while True:
         mpu.getdata()
-        print str(mpu.acc[0]) + ' ' + str(mpu.gyr[0])
+        for i in range(3):
+            print str(mpu.acc[i]) + ' ' + str(mpu.gyr[i])
         print mpu.calc_acc_deg()
-        time.sleep(1.0)
+        time.sleep(0.2)
